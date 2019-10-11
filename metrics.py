4ed654c6ac04ea5ba6616a951d235a56d917f86c
imkemayer <mayer.imke@gmail.com>
2019-10-11 21:59:55 +0200

    Add doubly robust estimator, OLS and modified OLS estimator for estimation of ATE

--- metrics.py
+++ metrics.py
@@ -1,5 +1,35 @@
-import numpyas np
+import numpy as np
 
-def tau_dr(xxx):
-    return 0
+# Doubly robust ATE estimation
+# if method == "glm": provide fitted values y1_hat, y0_hat and ps_hat
+#                     for the two response surfaces and the propensity scores respectively
+# if method == "grf": no need to provide any fitted values
+def tau_dr(Z_hat, w, y, method = "glm", y1_hat, y0_hat, ps_hat):
 
+	if method == "glm":
+		tau_i = y1_hat - y0_hat + w*(y-y1_hat)/ps_hat - 
+					(1-w)*(y-y0_hat)/(1-ps_hat)
+    	tau = mean(tau_i)
+    elif method == "grf":
+    	print("Causal forest estimation not implemented here yet.")
+
+    return tau
+
+# ATE estimation via OLS regression 
+def tau_ols(Z_hat, w, y):
+    ZW = np.concatenate((Z_hat, w), axis=1)
+    lr = LinearRegression()
+    lr.fit(ZW, y)
+    tau = lr.coef_[-1]
+
+    return tau
+
+# Difference with linear_tau: add estimated propensity 
+# scores as additional predictor
+def tau_ols_ps(Z_hat, w, y, ps_hat):
+    ZpsW = np.concatenate((Z_hat,ps_hat, w), axis=1)
+    lr = LinearRegression()
+    lr.fit(ZpsW, y)
+    tau = lr.coef_[-1]
+
+    return tau
