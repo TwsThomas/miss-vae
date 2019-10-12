322094feee1246b9d1dc0b5ed5291b8f9be4b876
twsthomas <twsthomas@gmail.com>
2019-10-12 11:27:05 +0200

    minor

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from metrics import tau_dr, tau_ols
 from generate_data import gen_lrmf, ampute
 
 
-def exp(n=1000, d=3, p=100, d_miwae=3, n_epochs=2):
+def exp(n=1000, d=3, p=100, d_miwae=3, n_epochs=1):
 
     Z, X, w, y, ps = gen_lrmf(n=n, d=d, p=p)
 
@@ -15,17 +15,33 @@ def exp(n=1000, d=3, p=100, d_miwae=3, n_epochs=2):
                                  n_epochs=n_epochs)
 
     print('shape of outputs miwae:')
-    print('xhat.shape, zhat.shape, zhat_mul.shape:')
+    print('xhat.shape, zhat.shape, zhat_mul.shape:') 
+    #    (1000, 200) (1000, 3) (200, 1000, 3)
     print(xhat.shape, zhat.shape, zhat_mul.shape)
 
-    tau_dr = tau_dr(Z_hat, w, y, "glm", y1_hat, y0_hat, ps_hat)
-    tau_ols = tau_ols(Z_hat, w, y)
+    res_tau_dr = 0 #tau_dr(zhat, w, y, "glm", y1_hat, y0_hat, ps_hat)
+    res_tau_ols = tau_ols(zhat, w, y)
 
-    print('tau_dr =', tau_dr)
-    print('tau_ols =', tau_ols)
+    print('tau_dr =', res_tau_dr)
+    print('tau_ols =', res_tau_ols)
 
-    return tau
+    return res_tau_dr, res_tau_ols
 
+
+def plot_n_d():
+
+    range_n = [10**4, 10**6]
+    range_p = [20,100, 1000]
+
+    l_scores = []
+    for n in range_n:
+        for p in range_p:
+            print(n, p)
+            l_scores.append(exp(n=n, d=3, p=p, d_miwae=3, n_epochs=2))
+            print('exp with (n,p)=',n,p,'........... DONE !')
+
+    np.savetxt(l_scores, 'results/plot_n_d.nptxt')
 
 if __name__ == '__main__':
-    exp()
+    plot_n_d()
+    #exp()
