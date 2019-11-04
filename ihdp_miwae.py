1ecc7a12919f216d736f15f3c2c792d19879229e
imkemayer <mayer.imke@gmail.com>
2019-11-04 10:10:09 +0100

    ihdp_04.1_11 (1000 samples)

--- ihdp_miwae.py
+++ ihdp_miwae.py
@@ -6,25 +6,25 @@ from main_ihdp import ihdp_miwae
 from config_ihdp import args
 
 # set here params 
-range_prop_miss = [0.5,] # [0.1, 0.3, 0]
+range_prop_miss = [0.1, 0.3, 0]
 range_sig_prior = [0.1, 1, 10]
 range_n_epochs = [10, 400]
 range_d_miwae = [10, 100]
 
-exp_name = 'ihdp_25.2_10'
+exp_name = 'ihdp_04.1_11'
 # 
 
 
 print('starting ihdp: ' + exp_name)
 
-l_tau = ['tau_dr', 'tau_ols', 'tau_ols_ps', 'mul_tau_dr', 'mul_tau_ols', 'mul_tau_ols_ps', 'dcor_zhat', 'dcor_zhat_mul']
+l_tau = ['tau_dr', 'tau_ols', 'tau_ols_ps','tau_resid', 'mul_tau_dr', 'mul_tau_ols', 'mul_tau_ols_ps','mul_tau_resid', 'dcor_zhat', 'dcor_zhat_mul']
 
 output = 'results/'+exp_name+'.csv'
 l_scores = []
 
-for args['set_id'] in range(1,11):
+for args['set_id'] in range(1,1001):
     for args['prop_miss'] in range_prop_miss:
-        for args['add_wy'] in [False, True]:
+        for args['add_wy'] in [False]:#, True]:
             for args['sig_prior'] in range_sig_prior:
                 for args['n_epochs'] in range_n_epochs:
                     for args['d_miwae'] in range_d_miwae:     
