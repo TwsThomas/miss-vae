5cc1e1fcec2dbd24378bfda9c9f532d9c0c50d7d
imkemayer <mayer.imke@gmail.com>
2019-11-07 15:24:12 +0100

    ihdp_07.2_11_cevae (larger prop_miss)

--- ihdp_cevae.py
+++ ihdp_cevae.py
@@ -7,9 +7,9 @@ from main_ihdp import ihdp_cevae
 from config_ihdp import args
 
 # set here params 
-range_prop_miss = [0.1, 0.3, 0]
+range_prop_miss = [0.5, 0.7, 0.9]#[0.1, 0.3, 0]
 
-exp_name = 'ihdp_07.1_11_cevae'
+exp_name = 'ihdp_07.2_11_cevae'
 # 
 
 
