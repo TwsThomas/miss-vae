1f17ac6b3629afec9b20f1db59c4e4ade085abbe
imkemayer <mayer.imke@gmail.com>
2019-11-07 15:33:04 +0100

    ihdp_07.2_11 (larger prop_miss)

--- ihdp_miwae.py
+++ ihdp_miwae.py
@@ -6,12 +6,12 @@ from main_ihdp import ihdp_miwae
 from config_ihdp import args
 
 # set here params 
-range_prop_miss = [0.1, 0.3, 0]
+range_prop_miss = [0.5, 0.7, 0.9]#[0.1, 0.3, 0]
 range_sig_prior = [0.1, 1, 10]
 range_n_epochs = [10,]# 400]
 range_d_miwae = [10, 100]
 
-exp_name = 'ihdp_07.1_11'
+exp_name = 'ihdp_07.2_11'
 # 
 
 
