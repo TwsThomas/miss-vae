43fefd4f8e52f04eda5a5987f910c88078dcc272
imkemayer <mayer.imke@gmail.com>
2019-11-11 12:23:11 +0100

    Revert "ihdp_cv_10.2_11 (larger prop_miss)"
    This reverts commit 22e57e13f5f0f706e97b5937e464fb8e63da35e2.

--- ihdp_miwae_cv.py
+++ ihdp_miwae_cv.py
@@ -6,12 +6,12 @@ from main_ihdp import ihdp_miwae_cv
 from config_ihdp import args
 
 # set here params 
-range_prop_miss = [0.5,]#[0.1, 0.3, 0]
+range_prop_miss = [0.1, 0.3, 0]
 range_sig_prior = [0.1, 1, 10]
 range_n_epochs = [100,] #[10,]# 400]
 range_d_miwae = [5, 10, 100]
 
-exp_name = 'ihdp_cv_10.2_11'
+exp_name = 'ihdp_cv_10.1_11'
 # 
 
 
